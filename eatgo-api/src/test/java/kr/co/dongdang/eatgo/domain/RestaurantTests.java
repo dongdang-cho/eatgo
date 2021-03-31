d1b71299b72d28d4d5f80e28100c2a546d424d76
dongdang <cdy235a@gmail.com>
2021-04-01 01:44:11 +0900

    ë 레스토랑 테ã스트

---
+++ eatgo-api/src/test/java/kr/co/dongdang/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,25 @@
+package kr.co.dongdang.eatgo.domain;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+@SpringBootTest
+public class RestaurantTests {
+
+    @Test
+    public void creation() {
+        Restaurant restaurant = new Restaurant("Bob zip","seoul");
+        assertThat(restaurant.getName(), is("Bob zip"));
+        assertThat(restaurant.getAddress(), is("seoul"));
+
+    }
+    @Test
+    public void information() {
+        Restaurant restaurant = new Restaurant("Bob zip","seoul");
+        assertThat(restaurant.getInformation(),is("Bob zip in seoul"));
+    }
+
+}
